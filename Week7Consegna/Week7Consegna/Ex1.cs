0f212b69222a2859dfb6c781b1db90d9f4cb9262
Valentyna <tytepon@gmail.com>
2021-03-12 13:12:46 +0100

    first commit

---
+++ Week7Consegna/Week7Consegna/Ex1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Week7
+{
+    public static class Ex1
+    {
+        public static void Run()
+        {
+            try
+            {
+                ExecuteSqlQuerry();
+            }
+            catch (SqlException e) when (e.Class > 19)
+            {
+                Console.WriteLine("abbiamo l'errore fatale");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Errore durante la query");
+
+            }
+        }
+
+        public static void ExecuteSqlQuerry()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Soure =.; Database=NONFUZIONA;Connection Timeunt = 1");
+            connection.Open();
+        }
+    }
+}
